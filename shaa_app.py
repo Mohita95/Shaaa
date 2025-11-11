64f570c0ea8136f97761c88cf1987ba5ec34752c
Mohita95 <159923969+Mohita95@users.noreply.github.com>
2025-11-11 02:45:20 -0600

    Update shaa_app.py

--- shaa_app.py
+++ shaa_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.set_page_config(page_title="For Shaa ??", page_icon="??", layout="centered")
-st.title("?? Open, Shaa — this is for you ??")
-st.write("Click the button below when you're ready...")
-if st.button("?? Open ??"):
+st.set_page_config(page_title="For Shaa", page_icon=":)", layout="centered")
+st.title("Smile & Click — this is for My Forever Baby, Shaa <3")
+st.write("Click the button below thinking about all your Powers!!!")
+if st.button("Works For Greek Gods Only"):
    st.image("https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif", use_container_width=True)
    st.markdown(
-       "<h2 style='text-align:center; color:#ff69b4;'>Shaa, jaldi theek hojaaaa! :) ????</h2>",
+       "<h2 style='text-align:center; color:#ff69b4;'>Mera Shaa, Tu jaldi theek hogaaaa! :) </h2>",
        unsafe_allow_html=True
    )
