95c5827e0dfe549f3410e3739ccc452f1dd4d315
Mohita95 <159923969+Mohita95@users.noreply.github.com>
2025-11-11 02:25:11 -0600

    Add files via upload

---
+++ shaa_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.set_page_config(page_title="For Shaa ??", page_icon="??", layout="centered")
+st.title("?? Open, Shaa — this is for you ??")
+st.write("Click the button below when you're ready...")
+if st.button("?? Open ??"):
+   st.image("https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif", use_container_width=True)
+   st.markdown(
+       "<h2 style='text-align:center; color:#ff69b4;'>Shaa, jaldi theek hojaaaa! :) ????</h2>",
+       unsafe_allow_html=True
+   )
